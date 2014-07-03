7f84b2ca190c757b9f60189467dd6d45630fb271
Slava Epifanov <slava.epifanov@gmail.com>
2014-07-03 13:45:38 +0400

    leads stuff was added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Travel::Application.routes.draw do
+  resources :leads
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ Travel::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'leads#index'
 
   # See how all your routes lay out with "rake routes"
 
