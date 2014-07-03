b5006528d46dfd3c36b3e01de45aa12079ed126f
Slava Epifanov <slava.epifanov@gmail.com>
2014-07-03 11:31:13 +0400

    improving deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,58 +1,46 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+server "217.23.138.165", user: "rj", roles: [:web, :app, :db], primary: true
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :application, 'seo_tool'
+set :repo_url, 'git@github.com:epifanov/travel.git'
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :shared_path, "#{deploy_to}/shared"
+set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
+set :rails_env, "production"
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :deploy_user, 'rj'
+set :rvm_ruby_version, "2.1.1"
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
   after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  
+  desc 'Start application'
+  task :start do
+    on roles(:app) do
+      execute "/etc/init.d/unicorn_#{fetch(:application)} start", raise_on_non_zero_exit: false
     end
   end
 
+  desc 'Stop application'
+  task :stop do
+    on roles(:app) do
+      execute "/etc/init.d/unicorn_#{fetch(:application)} stop", raise_on_non_zero_exit: false
+    end
+  end
+
+  desc 'Restart Application'
+  task :restart do
+    on roles(:app) do
+      execute "/etc/init.d/unicorn_#{fetch(:application)} restart", raise_on_non_zero_exit: false
+    end
+  end
+  
 end
+
+#after 'deploy:stop',    'delayed_job:stop'
+#after 'deploy:start',   'delayed_job:start'
+#before 'deploy:stop',"symlink:setup_config"
