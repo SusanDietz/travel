9218c3e4d3c4c91dc359667c3ea8dbdc7d708d74
T-V-N <dr.tvn@yandex.ru>
2014-07-17 17:20:34 +0400

    JS for map added

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+var directionsDisplay = new google.maps.DirectionsRenderer();
+var directionsService = new google.maps.DirectionsService();
+
+function calcRoute() {
+  var origin      = new google.maps.LatLng(41.850033, -87.6500523);
+  var destination = new google.maps.LatLng(42.850033, -85.6500523);
+  var request = {
+      origin:      origin,
+      destination: destination,
+      travelMode:  google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
+calcRoute();
+
+var handler = Gmaps.build('Google');
+handler.buildMap({ internal: {id: 'directions'}}, function(){
+  directionsDisplay.setMap(handler.getMap());
+  
+});
+});
+
+
+lat = position.coords.latitude;
+lng = position.coords.longitude;
+
+Gmaps.map.callback = function() {
+   Gmaps.map.createMarker({
+      Lat: lat,
+      Lng: lng, 
+      rich_marker: null, 
+      marker_picture: ""
+   });
+}
+
+alert(lat+' '+lng);
