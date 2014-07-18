391e856c318573efa1005c7df5be32000a86e542
T-V-N <dr.tvn@yandex.ru>
2014-07-18 14:29:46 +0400

    drawing added

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,27 +1,42 @@
 $( document ).ready(function() {
-  var directionsDisplay = new google.maps.DirectionsRenderer();
-  var directionsService = new google.maps.DirectionsService();
+  // var directionsDisplay = new google.maps.DirectionsRenderer();
+  // var directionsService = new google.maps.DirectionsService();
 
-    function calcRoute() {
-      var origin      = new google.maps.LatLng(53.182209, 45.004350);
-      var destination = new google.maps.LatLng(53.203139, 45.009586);
-      var request = {
-          origin:      origin,
-          destination: destination,
-          travelMode:  google.maps.TravelMode.DRIVING
-      };
-      directionsService.route(request, function(response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(response);
-        }
-      });
-  }
+  var mapOptions = {
+    center: new google.maps.LatLng(53.184660, 44.972893),
+    zoom: 19}
 
-  calcRoute();
+var map = new google.maps.Map(document.getElementById('map-canvas'),
+mapOptions);
 
-  var handler = Gmaps.build('Google');
-  handler.buildMap({ internal: {id: 'map-canvas'}}, function(){
-    directionsDisplay.setMap(handler.getMap());
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.MARKER,
+        google.maps.drawing.OverlayType.CIRCLE,
+        google.maps.drawing.OverlayType.POLYGON,
+        google.maps.drawing.OverlayType.POLYLINE,
+        google.maps.drawing.OverlayType.RECTANGLE
+      ]
+    },
+    markerOptions: {
+      icon: 'images/beachflag.png'
+    },
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
   });
+  drawingManager.setMap(map);
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
 });
 
