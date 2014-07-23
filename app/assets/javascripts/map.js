ab5c95f45163f07061a5df976138c8a52ef7c32f
T-V-N <dr.tvn@yandex.ru>
2014-07-23 12:13:30 +0400

    map works!

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,40 +1,31 @@
 $( document ).ready(function() {
   // var directionsDisplay = new google.maps.DirectionsRenderer();
   // var directionsService = new google.maps.DirectionsService();
+handler = Gmaps.build('Google');
+handler.buildMap({ provider: {}, internal: {id: 'map-canvas'}}, function(){
+  markers = handler.addMarkers([
+    {
+      "lat": 0,
+      "lng": 0,
+      "picture": {
+        "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
+        "width":  36,
+        "height": 36
+      },
+      "infowindow": "hello!"
+    }
+  ]);
+  handler.bounds.extendWith(markers);
+  handler.fitMapToBounds();
+});
 
   var mapOptions = {
     center: new google.maps.LatLng(53.184660, 44.972893),
     zoom: 19}
 
-var map = new google.maps.Map(document.getElementById('map-canvas'),
-mapOptions);
+// var map = new google.maps.Map(document.getElementById('map-canvas'),
+// mapOptions);
 
-  var drawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.MARKER,
-    drawingControl: true,
-    drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-        google.maps.drawing.OverlayType.MARKER,
-        google.maps.drawing.OverlayType.CIRCLE,
-        google.maps.drawing.OverlayType.POLYGON,
-        google.maps.drawing.OverlayType.POLYLINE,
-        google.maps.drawing.OverlayType.RECTANGLE
-      ]
-    },
-    markerOptions: {
-      icon: 'images/beachflag.png'
-    },
-    circleOptions: {
-      fillColor: '#ffff00',
-      fillOpacity: 1,
-      strokeWeight: 5,
-      clickable: false,
-      editable: true,
-      zIndex: 1
-    }
-  });
-  drawingManager.setMap(map);
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
