75f84311aa8c49ed3f09c89b62784041e8c00fad
T-V-N <dr.tvn@yandex.ru>
2014-07-29 13:19:22 +0400

    map and registration buttons were added and inserted to itinerary view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Travel::Application.routes.draw do
 
-  resources :itineraries
+
 
 
   get "omniauth_callbacks/facebook"
@@ -14,7 +14,7 @@ Travel::Application.routes.draw do
   resources :leads
   resources :markers
   resources :points
-
+  resources :itineraries
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -66,7 +66,7 @@ Travel::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-  root :to => 'points#index'
+  root :to => 'itineraries#index'
 
   # See how all your routes lay out with "rake routes"
 
