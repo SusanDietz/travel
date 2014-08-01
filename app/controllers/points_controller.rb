28c1566a1fcccb1f1229a1d18d3e6d015f122ef8
T-V-N <dr.tvn@yandex.ru>
2014-08-01 10:11:19 +0400

    Proper point creating and destroying

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -5,6 +5,16 @@ class PointsController < InheritedResources::Base
     @point = @itinerary.points.create(params[:point])
     respond_to do |format|
       format.js { }
+  end
+
+  def destroy
+    @point = Point.find(params[:id])
+    if @point.destroy
+      respond_to do |format|
+        format.js
+      end
     end
+  end
+
   end
 end
